fb8742d4823544cfb44cfa823c32f337bf2f3b48
singhrishabh93 <singhrishabh1670@gmal.com>
2023-04-16 12:52:01 +0530

    Add existing project files to Git

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,10 +61,14 @@ const Header = () => {
                 animate={{opacity : 1, x : 0}}
                 exit={{opacity : 0, x : 200}}
                 className="flex items-center gap-8">
-                    <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
-                    <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</li>
-                    <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About Us</li>
-                    <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Service</li>
+                    <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+                    onClick={() => setIsMenu(false)}>Home</li>
+                    <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+                    onClick={() => setIsMenu(false)}>Menu</li>
+                    <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+                    onClick={() => setIsMenu(false)}>About Us</li>
+                    <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+                    onClick={() => setIsMenu(false)}>Service</li>
                 </motion.ul>
 
                 <div className="relative flex items-center justify-center">
@@ -90,7 +94,7 @@ const Header = () => {
                         className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
                     {user && user.email === "singhrishabh1670@gmail.com" && (
                             <Link to={"/createItem"}>
-                            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base" onClick={() => setIsMenu(false)}>New Item <MdAdd /></p>
                             </Link>
                         )
                     }
